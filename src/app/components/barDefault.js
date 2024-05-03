ddb8e096670eb714773668f07bf2b592dab25d65
unknown <shomirsaidov.abubakr@mail.ru>
2024-05-03 11:51:01 +0500

    poet screen

--- src/app/components/barDefault.js
+++ src/app/components/barDefault.js
@@ -4,7 +4,7 @@ const TopBar = (props) => {
        <div className="fixed w-full border-b-2 p-4 bg-white" style={{zIndex: 5, top: '0px'}}>
 
             <div className="flex justify-between align-items-center">
-                <img src={'/assets/icons/profile-circle.svg'} className="cursor-pointer" width={35} height={35}/>
+                <img src={'/assets/icons/profile-circle.svg'} className="cursor-pointer" width={30} height={30}/>
                 <h1>{props.title}</h1>
                 <img src={'/assets/icons/search-normal-1.svg'} className="cursor-pointer"/>
             </div>
