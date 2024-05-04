64ff7f2abf0ed7f908d28bc3ccc4e02df547fada
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2024-05-04 11:46:22 +0500

    Update poem.js

--- src/app/components/poem.js
+++ src/app/components/poem.js
@@ -20,6 +20,81 @@ const Poem = (props) => {
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
+            <div className="flex justify-center px-16 mt-8">
+                <div>
+                    <div className="text-sm">
+                    Эй толиби дидори Хак ,бедор шав вакти сахар <br>, Чуяндаи асрори Хак ,бедор шав вакти сахар ,<br>
+Харчанд захмат мерасад ,бисёр неъмат мерасад ,<br>
+Акнун ,ки фурсат мерасад ,бедор шав вакти сахар .   
+                    </div>
+                    <button style={{color: '#6865FF'}}>...муфассал</button>
+
+                    <div className="flex justify-between mt-4">
+                        <div style={{color: '#6865FF'}}>02.04.2024</div>
+                        <div className="flex justify-between space-x-2">
+                            <img src={'/assets/icons/like.svg'} width={25} className="mb-2"/>
+                            <span>2345</span>
+                        </div>                
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="flex justify-center px-16 mt-8">
+                <div>
+                    <div className="text-sm">
+                    Бишнав аз най чун хикоят мекунад,<br>
+Аз чудоихо шикоят мекунад,<br>
+К-аз найистон то маро бибридаанд,<br>
+Дар нафирам марду зан нолидаанд.  
+                    </div>
+                    <button style={{color: '#6865FF'}}>...муфассал</button>
+
+                    <div className="flex justify-between mt-4">
+                        <div style={{color: '#6865FF'}}>02.04.2024</div>
+                        <div className="flex justify-between space-x-2">
+                            <img src={'/assets/icons/like.svg'} width={25} className="mb-2"/>
+                            <span>2345</span>
+                        </div>                
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="flex justify-center px-16 mt-8">
+                <div>
+                    <div className="text-sm">
+                   Хар марди шутурдор Увайси Карани нест,<br> Хар шишаи гулранг ъакики ямани нест.<br>
+Хар сангу гиле гавхари ноёб нагардад,<br>
+Хар Ахмаду Махмуд Расули мадани нест.<br>
+                    </div>
+                    <button style={{color: '#6865FF'}}>...муфассал</button>
+
+                    <div className="flex justify-between mt-4">
+                        <div style={{color: '#6865FF'}}>02.04.2024</div>
+                        <div className="flex justify-between space-x-2">
+                            <img src={'/assets/icons/like.svg'} width={25} className="mb-2"/>
+                            <span>2345</span>
+                        </div>                
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+    
         </>
     )
 }
