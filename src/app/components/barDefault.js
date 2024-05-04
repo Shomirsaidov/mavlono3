7c447452f1af2f562b05d9e6ea6d7ee2b3659bce
unknown <shomirsaidov.abubakr@mail.ru>
2024-05-04 13:20:25 +0500

    saasd

--- src/app/components/barDefault.js
+++ src/app/components/barDefault.js
@@ -4,7 +4,7 @@ const TopBar = (props) => {
        <div className="fixed w-full border-b-2 p-4 bg-white" style={{zIndex: 5, top: '0px'}}>
 
             <div className="flex justify-between align-items-center">
-                <a className="/profile">
+                <a href="/profile">
                     <img src={'/assets/icons/profile-circle.svg'} className="cursor-pointer" width={30} height={30}/>
                 </a>
                 <h1>{props.title}</h1>
